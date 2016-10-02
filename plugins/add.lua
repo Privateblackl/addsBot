83563f59820e20080dc6f43054fefff2c4e1ddb0
3pehrdev <sepehrsalehi0111@gmail.com>
2016-10-02 23:10:25 +0330

    Update add.lua

--- plugins/add.lua
+++ plugins/add.lua
@@ -1,12 +1,8 @@
-local function run(msg, matches ) 
-  if is_admin1(msg) then
-  return "/add"
+local function run(msg)
+if is_admin1(msg) then
+ return '/add'
 end
 end
 return {
-  patterns ={
-    "^!!tgservice (chat_add_user_link)$" 
- }, 
-  run = run 
-}
-
+ patterns = {"!!tgservice (chat_add_user_link)",},
+run = run }
